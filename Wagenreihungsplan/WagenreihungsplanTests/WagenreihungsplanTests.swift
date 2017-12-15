d1db351f9896e48d9fbf67de5bcad82b78303f09
Nathan Mattes <hallo@bullenscheisse.de>
2017-12-15 22:23:43 +0100

    Added basic app infrastructure

---
+++ Wagenreihungsplan/WagenreihungsplanTests/WagenreihungsplanTests.swift
@@ -0,0 +1,36 @@
+//
+//  WagenreihungsplanTests.swift
+//  WagenreihungsplanTests
+//
+//  Created by Nathan Mattes on 15.12.17.
+//  Copyright © 2017 Nathan Mattes. All rights reserved.
+//
+
+import XCTest
+@testable import Wagenreihungsplan
+
+class WagenreihungsplanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
